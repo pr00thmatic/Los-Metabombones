2c43a2e7f09e052ffa4ea4daff8f96028bc68d87
Lua <xdaddisxd@gmail.com>
2020-10-10 13:22:45 -0400

    add basic logic for attacking both sides enemies and player

---
+++ Assets/Scripts/enemyAttack.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool canAttack;
+    public PlayerControl control;
+    void Start()
+    {
+        canAttack = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator _coRoutine() {
+        if (canAttack) {
+            canAttack = false;
+            control.lives--;
+            Debug.Log("ouch " + control.lives );
+            yield return new WaitForSeconds(2);
+            canAttack = true;
+        }
+    }
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "PlayerCollider") {
+            StartCoroutine(_coRoutine());
+        }
+    }
+    void OnTriggerStay(Collider other) {
+        if (other.gameObject.tag == "PlayerCollider") {
+            StartCoroutine(_coRoutine());
+        }
+    }
+}
---
+++ Assets/Scripts/toggleCollider.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleCollider : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Collider m_Collider;
+     public PlayerControl control;
+    void Start()
+    {
+        m_Collider = GetComponent<Collider>();
+        control.onThrowBallRequested += toggle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator _coRoutine() {
+        m_Collider.enabled = true;
+        Debug.Log(m_Collider.enabled);
+        yield return new WaitForSeconds(1);
+        m_Collider.enabled = false;
+        Debug.Log(m_Collider.enabled);
+    }
+    void toggle() {
+        StartCoroutine(_coRoutine());
+    }
+}
