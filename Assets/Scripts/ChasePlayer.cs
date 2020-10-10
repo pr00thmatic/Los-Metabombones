75f6539331484caabde7e78263246a06f43447ef
Lua <xdaddisxd@gmail.com>
2020-10-10 18:46:16 -0400

    chase  player

---
+++ Assets/Scripts/ChasePlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChasePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    private Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 direction = player.position - transform.position;
+        Quaternion angle =  Quaternion.LookRotation(direction, Vector3.up);
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+    private void FixedUpdate() {
+        moveCharacter(movement);
+    }
+    void moveCharacter(Vector3 direction){
+        Debug.Log(direction);
+        rb.MovePosition(transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
