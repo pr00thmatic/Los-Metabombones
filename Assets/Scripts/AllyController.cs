fe7d9d64e02caed121c50f16c81ff4570a6a8f4b
Rolando Troche Venegas <tototroche24@gmail.com>
2020-10-11 18:25:59 -0400

    level 2 boss

---
+++ Assets/Scripts/AllyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllyController : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+        
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+        
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    
+    if(other.gameObject.tag == "Virus")
+    {
+      transform.Find("Healthy").gameObject.SetActive(false);
+      transform.Find("Sick").gameObject.SetActive(true);
+      other.gameObject.SetActive(false);
+    }
+  }
+}
