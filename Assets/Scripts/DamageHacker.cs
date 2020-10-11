025d6419887e06f5b7c5c87f082813c33217958b
Lua <xdaddisxd@gmail.com>
2020-10-11 17:17:36 -0400

    ade many enemies for hacker level

---
+++ Assets/Scripts/DamageHacker.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageHacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+     public HackerBossLife control;
+     public GameObject parent;
+     
+    void Start()
+    {
+        control = HackerBossLife.Instance;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball") {
+            control.anonymusLife = control.anonymusLife - 1;
+            Debug.Log(control.anonymusLife);
+            Destroy(parent);
+        }
+    }
+}
---
+++ Assets/Scripts/HealOrDamageYou.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealOrDamageYou : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    
+    public PlayerControl control;
+    public GameObject parent;
+    void Start()
+    {
+        control = PlayerControl.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+      void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball") {
+            control.lives = control.lives - 1;
+            Destroy(parent);
+        }
+        if (other.gameObject.tag == "PlayerCollider") {
+            control.lives = control.lives + 1;
+            Destroy(parent);
+        }
+    }
+}
