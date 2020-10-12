5f0150905bb7f89e29559790f44b1220fc2bc513
Rolando Troche Venegas <tototroche24@gmail.com>
2020-10-11 21:23:29 -0400

    covid level with enemys

--- Assets/Scripts/AllyController.cs
+++ Assets/Scripts/AllyController.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class AllyController : MonoBehaviour
 {
+  private Transform player;
+  public float minDistance;
   // Start is called before the first frame update
   void Start()
   {
-        
+    player = PlayerControl.Instance.transform;
   }
 
   // Update is called once per frame
   void Update()
   {
-        
+    if (player != null && Vector3.Distance(player.position, transform.position) < minDistance)
+    {
+      transform.Find("Healthy").gameObject.SetActive(false);
+      transform.Find("Sick").gameObject.SetActive(true);
+    }
   }
 
   private void OnTriggerEnter(Collider other)
@@ -22,6 +28,7 @@ public class AllyController : MonoBehaviour
     if(other.gameObject.tag == "Virus")
     {
       transform.Find("Healthy").gameObject.SetActive(false);
+      Destroy(transform.Find("Healthy").gameObject);
       transform.Find("Sick").gameObject.SetActive(true);
       other.gameObject.SetActive(false);
     }
